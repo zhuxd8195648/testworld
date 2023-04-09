55afcde81f1d356f803d0b96a01e33bf06079485
test8195648 <8195648@qq.com>
2023-04-09 23:34:33 +0800

    20230409

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyCompany("Ludeon Studios")]
+[assembly: AssemblyCopyright("Copyright Ludeon Studios.")]
+[assembly: AssemblyTrademark("RimWorld is a registered trademark of Ludeon Studios.")]
+[assembly: AssemblyVersion("1.2.7558.21451")]
---
+++ README.md
@@ -0,0 +1,12 @@
+# RimWorldDecompiled
+
+模拟游戏RimWorld的反编译源代码(dll)(只允许个人使用)  
+ 
+我不拥有这份回购书中的任何代码。 几乎所有的版权都归Ludeon Studios所有。  
+ 
+这段代码有很多许可证，其中一些列在licenses .txt下面。  
+代码的某些部分来自第三方，并在他们自己的许可下列出。  
+ 
+但大部分代码由Ludeon Studios拥有完整版权。  
+
+RimWorld's [EULA](https://rimworldgame.com/eula/) 允许你反编译游戏供个人使用。  
